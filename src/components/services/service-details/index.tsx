c17030173798c3d6451e6e9b6e6f082bc653ac73
Cristi Radu <cristi@triplemars.com>
2025-07-10 18:33:56 +0300

    Services

--- src/components/services/service-details/index.tsx
+++ src/components/services/service-details/index.tsx
@@ -1,19 +1,26 @@
-import FooterThree from "../../../layouts/footers/FooterThree"
-import HeaderThree from "../../../layouts/headers/HeaderThree"
-import BreadCrumb from "../../common/BreadCrumb"
-import ServiceDetailsArea from "./ServiceDetailsArea"
+import { useParams } from "react-router-dom";
+import FooterThree from "../../../layouts/footers/FooterThree";
+import HeaderThree from "../../../layouts/headers/HeaderThree";
+import BreadCrumb from "../../common/BreadCrumb";
+import ServiceDetailsArea from "./ServiceDetailsArea";
+
+const serviceMap: { [key: string]: string } = {
+  "industrial-textile-manufacturing": "Industrial Textile Manufacturing",
+  "plastic-molding": "Plastic Molding",
+};
 
 const ServiceDetails = () => {
-   return (
-      <>
-         <HeaderThree />
-         <main>
-            <BreadCrumb sub_title="Service Details" title="Service Details" />
-            <ServiceDetailsArea />
-         </main>
-         <FooterThree />
-      </>
-   )
-}
+  const { serviceName } = useParams();
+  return (
+    <>
+      <HeaderThree />
+      <main>
+        <BreadCrumb sub_title={serviceMap[serviceName?.toLowerCase() ?? ""]} title={serviceMap[serviceName?.toLowerCase() ?? ""]} />
+        <ServiceDetailsArea />
+      </main>
+      <FooterThree />
+    </>
+  );
+};
 
-export default ServiceDetails
+export default ServiceDetails;
--- src/pages/ServiceDetailsMain.tsx
+++ src/pages/ServiceDetailsMain.tsx
@@ -1,14 +1,22 @@
-import Wrapper from '../layouts/Wrapper';
-import SEO from '../components/SEO';
-import ServiceDetails from '../components/services/service-details';
+import { useParams } from "react-router-dom";
+import Wrapper from "../layouts/Wrapper";
+import SEO from "../components/SEO";
+import ServiceDetails from "../components/services/service-details";
+
+const serviceMap: { [key: string]: string } = {
+  "industrial-textile-manufacturing": "Industrial Textile Manufacturing",
+  "plastic-molding": "Plastic Molding",
+};
 
 const ServiceDetailsMain = () => {
-   return (
-      <Wrapper>
-         <SEO pageTitle={'Service Details'} />
-         <ServiceDetails />
-      </Wrapper>
-   );
+  const { serviceName } = useParams();
+
+  return (
+    <Wrapper>
+      <SEO pageTitle={serviceMap[serviceName?.toLowerCase() ?? ""]} />
+      <ServiceDetails />
+    </Wrapper>
+  );
 };
 
 export default ServiceDetailsMain;
